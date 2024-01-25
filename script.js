234c8e0037a9fe66f0572515f017624eb9856b91
mubashirnazir42 <153961579+mubashirnazir42@users.noreply.github.com>
2024-01-25 18:25:34 +0500

    firstCommit

---
+++ script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+// var firstName = "danda";
+
+// const jonas = {
+//   firstName: "mubashir",
+//   year: 1994,
+//   printName: function () {
+//     console.log(`my name is ${this.firstName}`);
+//   },
+//   printArrowFunction: function () {
+//     console.log(`my name is ${this.firstName}`);
+//   },
+// };
+
+// jonas.printName();
+// jonas.printArrowFunction();
+
+// const jonas = {
+//   firstName: "mubashir",
+//   year: 1994,
+//   printName: function () {
+//     console.log(`my name is ${this.firstName}`);
+
+//     const printNamed = () => {
+//       console.log(`my name is ${this.firstName}`);
+//       console.log(`my name is ${this.year}`);
+//     };
+//     printNamed();
+//   },
+//   printArrowFunction: function () {
+//     console.log(`my name is ${this.firstName}`);
+//   },
+// };
+
+// jonas.printName();
+// const jonas = {
+//   firstName: "mubashir",
+//   year: 1994,
+//   printName: function () {
+//     console.log(`my name is ${this.firstName}`);
+//   },
+
+//   printNamed: function () {
+//     console.log(`my name is ${this.firstName}`);
+//     console.log(`my name is ${this.year}`);
+//   },
+
+//   printArrowFunction: function () {
+//     console.log(`my name is ${this.firstName}`);
+//   },
+// };
+
+// jonas.printNamed();
+
+// const addArgument = function (a, b, c) {
+//   console.log(arguments);
+// };
+
+// addArgument(12, 3, 44);
+
+// const AddarrowArguments = (number, mul) => {
+//   console.log(arguments);
+// };
+
+// AddarrowArguments(1, 2);
+// var number = 22;
+// const addNumberofExceution = (aaaaa, resisNumber) => {
+//   //   console.log(arguments);
+//   console.log(aaaaa + resisNumber / 2);
+// };
+// addNumberofExceution(422, 2);
